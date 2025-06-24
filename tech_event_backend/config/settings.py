454b3ceaf69d10038435ff5b9ef7103728381be8
Muhadev <muhammedfayemi@gmail.com>
2025-06-24 16:22:08 +0100

    fixing the package for the project

--- tech_event_backend/config/settings.py
+++ tech_event_backend/config/settings.py
@@ -205,7 +205,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
