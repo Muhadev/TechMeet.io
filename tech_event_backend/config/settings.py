08a7738829e2895a7a72bb086ffdd3dfd38c14cf
Muhadev <muhammedfayemi@gmail.com>
2025-06-24 15:45:58 +0100

    Switch to mysql-connector-python for Railway compatibility

--- tech_event_backend/config/settings.py
+++ tech_event_backend/config/settings.py
@@ -212,7 +212,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
         'OPTIONS': {
-            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
